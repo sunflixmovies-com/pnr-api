49d7615a9a50bcd8027f5e296e9edd889ca1bf27
sunflixmovies-com <sunflixmovies@gmail.com>
2026-04-03 03:02:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
 
 // ✅ PNR API route
 app.get('/pnr-status', async (req, res) => {
+  res.setHeader("Cache-Control", "no-store");
   const pnr = req.query.pnr;
 
   if (!pnr) {
